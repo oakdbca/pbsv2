7946e94a450f3ab5c0cd01377ea0aeebb99d6914
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-11 17:03:41 +0800

    Resolve gunicorn warning about configuration file needing a python extension.

--- pre_startup.sh
+++ pre_startup.sh
@@ -6,7 +6,7 @@ cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 # elevation permissions required
 sudo /startup.sh
-poetry run gunicorn govapp.wsgi --bind :8080 --config /app/gunicorn.ini
+poetry run gunicorn govapp.wsgi --bind :8080 --config /app/gunicorn.conf.py
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start gunicorn: $status"
