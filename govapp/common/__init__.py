42305c5c114e831af716264927ef644fb1e28390
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-12 16:32:22 +0800

    Remove non existing reference to sharepoint module.

--- govapp/common/__init__.py
+++ govapp/common/__init__.py
@@ -1,3 +1,3 @@
 """Provides Common Utilities, Helpers and Mixins for Prescribed Burns System Apps."""
 
-from . import azure, mixins, sharepoint, utils  # noqa: F401
+from . import azure, mixins, utils  # noqa: F401
