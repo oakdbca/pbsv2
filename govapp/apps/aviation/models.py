5ac69740317a7200b7d635445d5fd17f730ed24f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-13 12:36:01 +0800

    Remove integers from Choices (was causing errors). Change duration to decimal field. Add decision fields. Add approve, decline and user_is_assignable methods.

--- govapp/apps/aviation/models.py
+++ govapp/apps/aviation/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 from model_utils import Choices
-from model_utils.fields import StatusField
-from model_utils.models import TimeStampedModel
+from model_utils.models import StatusModel, TimeStampedModel
 
 from govapp.apps.main.models import AssignableModel, NameableModel, ReferenceableModel
 
@@ -18,12 +19,15 @@ class AircraftRegistration(NameableModel):
     pass
 
 
-class AviationRequest(ReferenceableModel, AssignableModel, TimeStampedModel):
+class AviationRequest(
+    StatusModel, ReferenceableModel, AssignableModel, TimeStampedModel
+):
+    MODEL_PREFIX = "AR"
     STATUS = Choices(
-        (0, "draft", "Draft"),
-        (1, "submitted", "Submitted"),
-        (2, "declined", "Declined"),
-        (3, "approved", "Approved"),
+        ("draft", "Draft"),
+        ("submitted", "Submitted"),
+        ("declined", "Declined"),
+        ("approved", "Approved"),
     )
 
     ignition_method = models.ForeignKey(
@@ -36,5 +40,35 @@ class AviationRequest(ReferenceableModel, AssignableModel, TimeStampedModel):
         AircraftRegistration, on_delete=models.PROTECT, null=False, blank=False
     )
     datetime_requested = models.DateTimeField(null=False, blank=False)
-    duration = models.DurationField(null=False, blank=False)
-    status = StatusField()
+    duration = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, blank=False
+    )
+    decision_made_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="aviation_request_decisions",
+    )
+    decision_datetime = models.DateTimeField(null=True, blank=True)
+    decision_text = models.TextField(null=True, blank=True)
+
+    def approve(self, decision_made_by: User, approve_text: str) -> None:
+        self.status = self.STATUS.approved
+        self.decision_made_by = decision_made_by
+        self.decision_datetime = timezone.now()
+        self.decision_text = approve_text
+        # Todo: Take other actions as required (i.e. email notifications, etc.)
+        self.save()
+
+    def decline(self, decision_made_by: User, decline_text: str) -> None:
+        self.status = self.STATUS.declined
+        self.decision_made_by = decision_made_by
+        self.decision_datetime = timezone.now()
+        self.decision_text = decline_text
+        # Todo: Change related implementation plan status back to draft
+        self.save()
+
+    def user_is_assignable(self, user: User) -> tuple[bool, str]:
+        # Todo - implement this method
+        return super().user_is_assignable(user)
