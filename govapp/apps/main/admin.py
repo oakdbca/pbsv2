d253e71db28b7a9f164aa33edd3ec09ae416cfa3
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-13 16:05:27 +0800

    Add basic admin for region and district.

--- govapp/apps/main/admin.py
+++ govapp/apps/main/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from .models import ModelFile
+from .models import District, ModelFile, Region
 
 
 class ModelFileAdmin(admin.ModelAdmin):
     list_display = ("id", "file", "content_type", "object_id", "datetime_uploaded")
 
 
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "display_name")
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "display_name")
+
+
 admin.site.register(ModelFile, ModelFileAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(District, DistrictAdmin)
