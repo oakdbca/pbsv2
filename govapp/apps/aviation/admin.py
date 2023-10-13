40b9cef521390d84806b3630372427bd9c62b416
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-13 12:34:39 +0800

    Add some basic admins for the aviation app.

--- govapp/apps/aviation/admin.py
+++ govapp/apps/aviation/admin.py
@@ -1,3 +1,36 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import AircraftRegistration, AircraftType, AviationRequest, IgnitionMethod
+
+
+class IgnitionMethodAdmin(admin.ModelAdmin):
+    pass
+
+
+class AircraftTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class AircraftRegistrationAdmin(admin.ModelAdmin):
+    pass
+
+
+class AviationRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "reference_number",
+        "status",
+        "ignition_method",
+        "aircraft_type",
+        "aircraft_registration",
+        "datetime_requested",
+        "duration",
+        "assigned_to",
+        "decision_made_by",
+        "decision_datetime",
+    )
+
+
+admin.site.register(IgnitionMethod, IgnitionMethodAdmin)
+admin.site.register(AircraftType, AircraftTypeAdmin)
+admin.site.register(AircraftRegistration, AircraftRegistrationAdmin)
+admin.site.register(AviationRequest, AviationRequestAdmin)
