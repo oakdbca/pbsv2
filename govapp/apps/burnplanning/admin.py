a9d54846b29c8213588271d42986a72bf86c3ca9
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-16 12:23:44 +0800

    Create basic admins for burn plan element models.

--- govapp/apps/burnplanning/admin.py
+++ govapp/apps/burnplanning/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from govapp.apps.main.admin import DeleteRestrictedAdmin
 
-from .models import BurnPlanUnit, BurnPlanUnitDistrict
+from .models import (
+    BurnPlanElement,
+    BurnPlanUnit,
+    BurnPlanUnitDistrict,
+    Justification,
+    OutputLeaderType,
+    Program,
+    Purpose,
+    Treatment,
+)
 
 
 class BurnPlanUnitDistrictInline(admin.TabularInline):
@@ -14,8 +23,23 @@ class BurnPlanUnitAdmin(admin.ModelAdmin):
     inlines = [BurnPlanUnitDistrictInline]
 
 
-class BurnPlanUnitDistrictAdmin(DeleteRestrictedAdmin):
-    pass
+class OutputLeaderInline(admin.TabularInline):
+    model = BurnPlanElement.output_leaders.through
+    verbose_name = "Output Leader"
+    verbose_name_plural = "Output Leaders"
+    extra = 0
+
+
+class BurnPlanElementAdmin(DeleteRestrictedAdmin):
+    inlines = [OutputLeaderInline]
+    exclude = ("output_leaders",)
 
 
 admin.site.register(BurnPlanUnit, BurnPlanUnitAdmin)
+admin.site.register(BurnPlanElement, BurnPlanElementAdmin)
+
+admin.site.register(Treatment)
+admin.site.register(Justification)
+admin.site.register(Purpose)
+admin.site.register(Program)
+admin.site.register(OutputLeaderType)
