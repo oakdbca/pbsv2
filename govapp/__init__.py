c9d5a4fe225084cfd1a6bdde2239ac509d844624
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-10-20 11:39:57 +0800

    Set up mypy checks to run automatically with the runserver command.

--- govapp/__init__.py
+++ govapp/__init__.py
@@ -1,1 +1,6 @@
 """The Prescribed Burns System DBCA Django Project."""
+import importlib.util
+
+mypy_package = importlib.util.find_spec("mypy")
+if mypy_package:
+    from .checks import mypy  # noqa: F401
