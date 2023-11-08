47313acf1362b1ade59ad7c4f862e0cb2bc17db5
Karsten Prehn <karsten.prehn@gmail.com>
2023-11-08 03:55:09 +0000

    Update to operational area programm and purpose & program migration files

--- govapp/apps/operationalarea/models.py
+++ govapp/apps/operationalarea/models.py
@@ -110,12 +110,18 @@ class OperationalArea(ReferenceableModel, UniqueNameableModel, TimeStampedModel)
     operation_name = models.CharField(
         max_length=255, null=True, blank=True
     )  # To be pre-filled with the name of the burn plan unit
-    # Purpose and Program come from the BPU?
     purpose: models.ManyToManyField = models.ManyToManyField(
         "burnplanning.Purpose",
         related_name="operational_areas",
         through="OperationalAreaPurpose",
         through_fields=("operational_area", "purpose"),
+        editable=False,
+    )
+    program: models.ManyToManyField = models.ManyToManyField(
+        "burnplanning.Program",
+        related_name="operational_areas",
+        through="OperationalAreaProgram",
+        through_fields=("operational_area", "program"),
         editable=False,
     )
     burn_priority = models.IntegerField(null=True, blank=True)
@@ -137,13 +143,6 @@ class OperationalArea(ReferenceableModel, UniqueNameableModel, TimeStampedModel)
 
     def __str__(self):
         return f"{self.reference_number} ({self.name})"
-
-    @property
-    def program(self):
-        if self.burn_plan_unit and self.burn_plan_unit.programs.exists():
-            # TODO How to get the selected program?
-            return self.burn_plan_unit.programs.first().program
-        return None
 
     def copy(self):
         self.pk = None
