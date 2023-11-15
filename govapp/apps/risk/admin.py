29e1f9fae7531ba7d67bf77cd7197eaa21be7209
Karsten Prehn <karsten.prehn@gmail.com>
2023-11-15 07:55:49 +0000

    Added contributing factor standard control model plus admin

--- govapp/apps/risk/admin.py
+++ govapp/apps/risk/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from govapp.apps.main.admin import DeleteRestrictedAdmin
 
-from .models import ContributingFactor, RiskCategory
+from .models import (
+    ContributingFactor,
+    ContributingFactorStandardControl,
+    RiskCategory,
+    StandardControl,
+)
+
+
+@admin.register(StandardControl)
+class StandardControlAdmin(DeleteRestrictedAdmin):
+    model = StandardControl
+    list_display = ("name",)
+
+
+class ContributingFactorStandardControlInline(admin.TabularInline):
+    model = ContributingFactorStandardControl
+    extra = 0
 
 
 @admin.register(ContributingFactor)
@@ -12,6 +28,10 @@ class ContributingFactorAdmin(DeleteRestrictedAdmin):
         "name",
         "factors",
     )
+
+    inlines = [
+        ContributingFactorStandardControlInline,
+    ]
 
 
 @admin.register(RiskCategory)
--- govapp/apps/risk/models.py
+++ govapp/apps/risk/models.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 from django.contrib.postgres.fields import DecimalRangeField
+from django.db import models
 from model_utils.models import TimeStampedModel
 
 from govapp.apps.main.models import UniqueNameableModel
@@ -12,8 +13,28 @@ class StandardControl(UniqueNameableModel, TimeStampedModel):
     pass
 
 
+class ContributingFactorStandardControl(models.Model):
+    contributing_factor = models.ForeignKey(
+        "ContributingFactor",
+        on_delete=models.PROTECT,
+        related_name="contributing_factor_standard_controls",
+    )
+    standard_control = models.ForeignKey(
+        "StandardControl",
+        on_delete=models.PROTECT,
+        related_name="contributing_factor_standard_controls",
+    )
+    revisit_in_implementation_plan = models.BooleanField(default=False)
+
+
 class ContributingFactor(UniqueNameableModel, TimeStampedModel):
     factors = DecimalRangeField(default_bounds="[)", blank=True, null=True)  # type: ignore
+    standard_controls: models.ManyToManyField = models.ManyToManyField(
+        StandardControl,
+        related_name="contributing_factors",
+        through="ContributingFactorStandardControl",
+        through_fields=("contributing_factor", "standard_control"),
+    )
 
 
 class RiskCategory(UniqueNameableModel, TimeStampedModel):
