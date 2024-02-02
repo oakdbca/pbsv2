eef47c1533de1d8130cfe16a0b614428721c4916
Karsten Prehn <karsten.prehn@gmail.com>
2024-02-02 06:21:52 +0000

    Added simple base view and burn plan element view

--- govapp/apps/burnplanning/views.py
+++ govapp/apps/burnplanning/views.py
@@ -2,6 +2,8 @@
 from django.db.models.query import QuerySet
 from rest_framework import viewsets
 
+from govapp.common.views import BaseView
+
 from .models import BurnPlanElement
 from .serializers import BurnPlanElementSerializer
 
@@ -9,10 +11,16 @@ from .serializers import BurnPlanElementSerializer
 class BurnPlanElementViewSet(viewsets.ModelViewSet):
     queryset = BurnPlanElement.objects.none()
     serializer_class = BurnPlanElementSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated] # TODO
 
     def get_queryset(self) -> QuerySet:
         pk = self.kwargs.get("pk", None)
         if pk:
             return BurnPlanElement.objects.filter(pk=pk)
         return BurnPlanElement.objects.all()
+
+
+class BurnPlanElementView(BaseView):
+    """Burn Plan Element page view."""
+
+    model = BurnPlanElement
