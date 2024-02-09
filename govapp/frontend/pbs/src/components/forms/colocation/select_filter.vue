0da1f4b3a31d779a50c63d75fcef490d07387678
Karsten Prehn <karsten.prehn@gmail.com>
2024-02-09 06:06:55 +0000

    Added single filter component

---
+++ govapp/frontend/pbs/src/components/forms/colocation/select_filter.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="row mb-2 p-2">
+        <div class="col-md-3">
+            <div class="form-group">
+                <label for="select-filter">{{ title }}</label>
+                <select
+                    id="select-filter"
+                    v-model="selectedFilterItem"
+                    class="form-control"
+                    @change="
+                        $emit('selection-changed', {
+                            id: id,
+                            value: selectedFilterItem,
+                        })
+                    "
+                >
+                    <option value="all">All</option>
+                    <option
+                        v-for="option in filterOptions"
+                        :key="option.value"
+                        :value="option.value"
+                    >
+                        {{ option.text }}
+                    </option>
+                </select>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectFilter',
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        filterOptions: {
+            type: Object,
+            required: true,
+        },
+    },
+    emits: ['selection-changed'],
+    data: function () {
+        return {
+            // TODO: Get from session storage
+            selectedFilterItem: 'all',
+        };
+    },
+};
+</script>
