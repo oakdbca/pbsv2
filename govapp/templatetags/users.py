5ad25055acdea3059c2b1988a5e32efe924ae813
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-13 15:27:42 +0800

    Add users.py.

--- govapp/templatetags/users.py
+++ govapp/templatetags/users.py
@@ -1,8 +1,21 @@
 """Django Users Template Tag."""
 
-
 # Third-Party
 from django import template
 
+from govapp import helpers
+
 # Register Template Tag
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def is_internal(context):
+    request = context["request"]
+    return helpers.is_internal(request)
+
+
+@register.simple_tag(takes_context=True)
+def is_django_admin(context):
+    request = context["request"]
+    return helpers.is_django_admin(request.user)
