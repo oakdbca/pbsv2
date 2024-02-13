7f27cf22360b5b167bb9595be13661c9270fadc2
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-13 15:27:32 +0800

    Add helpers.py.

--- govapp/helpers.py
+++ govapp/helpers.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+
+
 def is_member_of(user, group_name):
     # Todo: Consider caching the groups
     return user.is_superuser or user.groups.filter(name=group_name).exists()
@@ -5,3 +8,7 @@ def is_member_of(user, group_name):
 
 def is_internal(user):
     return user.is_superuser or user.is_staff
+
+
+def is_django_admin(user):
+    return is_member_of(settings.DJANGO_ADMIN)
