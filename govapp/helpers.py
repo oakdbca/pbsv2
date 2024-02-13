216c9d66568d8ba26bdda4b68663e465652840f2
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-13 16:30:37 +0800

    Add warning if group doesn't exist.

--- govapp/helpers.py
+++ govapp/helpers.py
@@ -1,8 +1,16 @@
+import logging
+
 from django.conf import settings
+from django.contrib.auth.models import Group
+
+logger = logging.getLogger(__name__)
 
 
 def is_member_of(user, group_name):
     # Todo: Consider caching the groups
+    if not Group.objects.filter(name=group_name).exists():
+        logger.warning(f"Group {group_name} does not exist")
+        return False
     return user.is_superuser or user.groups.filter(name=group_name).exists()
 
 
