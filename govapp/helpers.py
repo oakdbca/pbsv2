1300a2412bf5be7120817dde17faf17d87b294c8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-14 15:56:17 +0800

    Add cached_groups function.

--- govapp/helpers.py
+++ govapp/helpers.py
@@ -2,16 +2,31 @@ import logging
 
 from django.conf import settings
 from django.contrib.auth.models import Group
+from django.core.cache import cache
 
 logger = logging.getLogger(__name__)
 
 
+def cached_groups():
+    # Try to get groups from cache
+    groups = cache.get("groups")
+
+    # If groups are not in cache, fetch them from the database and store them in cache
+    if groups is None:
+        groups = Group.objects.all()
+        cache.set("groups", groups)
+
+    return groups
+
+
 def is_member_of(user, group_name):
-    # Todo: Consider caching the groups
-    if not Group.objects.filter(name=group_name).exists():
+    groups = cached_groups()
+    if not groups.filter(name=group_name).exists():
         logger.warning(f"Group {group_name} does not exist")
         return False
-    return user.is_superuser or user.groups.filter(name=group_name).exists()
+    return (
+        user.is_superuser or user.profile.cached_groups.filter(name=group_name).exists()
+    )
 
 
 def is_internal(user):
@@ -19,4 +34,4 @@ def is_internal(user):
 
 
 def is_django_admin(user):
-    return is_member_of(settings.DJANGO_ADMIN)
+    return is_member_of(user, settings.DJANGO_ADMIN)
