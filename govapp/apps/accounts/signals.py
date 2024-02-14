b91431df7fbbcd399b9767c5034ce9bfbee188ee
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-14 15:55:01 +0800

    Add signals to create a user profile when creating or saving the user model.

---
+++ govapp/apps/accounts/signals.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import (  # Import the built-in User model, which is a sender
+    User,
+)
+from django.db.models.signals import (  # Import a post_save signal when a user is created
+    post_save,
+)
+from django.dispatch import receiver  # Import the receiver
+
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    if not hasattr(instance, "profile") or not instance.profile:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
