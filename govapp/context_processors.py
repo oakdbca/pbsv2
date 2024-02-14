c225915d824a37b1a21f97deb2beba80f194dc60
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-14 11:15:36 +0800

    Further configuration.

--- govapp/context_processors.py
+++ govapp/context_processors.py
@@ -1,9 +1,10 @@
 # Third-Party
-from django import conf
-from django.core.cache import cache
+from typing import Any
+
+from django import conf, http
 
 
-def variables(request):  # (request: http.HttpRequest) -> dict[str, Any]:
+def variables(requestrequest: http.HttpRequest) -> dict[str, Any]:
     """Constructs a context dictionary to be passed to the templates.
 
     Args:
@@ -12,43 +13,13 @@ def variables(request):  # (request: http.HttpRequest) -> dict[str, Any]:
     Returns:
         dict[str, Any]: Context for the templates.
     """
-    # Construct and return context
-    is_django_admin = False
-    is_admin = False
-
-    if request.session.session_key is not None:
-        is_django_admin = cache.get(
-            "is_django_admin" + str(request.session.session_key)
-        )
-        is_admin = cache.get("is_admin" + str(request.session.session_key))
-
-        if is_django_admin is None:
-            try:
-                is_django_admin = request.user.groups.filter(
-                    name="Django Admin"
-                ).exists()
-            except Exception as e:
-                print(e)
-            cache.set(
-                "is_django_admin" + str(request.session.session_key),
-                is_django_admin,
-                86400,
-            )
-        if is_admin is None:
-            try:
-                is_admin = request.user.groups.filter(name="Administrators").exists()
-            except Exception as e:
-                print(e)
-
-            cache.set("is_admin" + str(request.session.session_key), is_admin, 86400)
 
     return {
         "template_group": "pbs",
         "template_title": "",
         "build_tag": conf.settings.BUILD_TAG,
         "GIT_COMMIT_HASH": conf.settings.GIT_COMMIT_HASH,
+        "GIS_SERVER_URL": conf.settings.GIS_SERVER_URL,
         "DJANGO_SETTINGS": conf.settings,
         "settings": conf.settings,
-        "is_django_admin": is_django_admin,  # request.user.groups.filter(name="Django Admin").exists(),
-        "is_admin": is_admin,  # request.user.groups.filter(name="Administrators").exists(),
     }
