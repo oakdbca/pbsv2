ea6ce99c77e687cb5546daecde0ddacb4c8c6606
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-15 14:48:26 +0800

    Remove datatables imports from main.js as they are now in the datatable template component.

--- govapp/frontend/pbs/src/main.js
+++ govapp/frontend/pbs/src/main.js
@@ -4,23 +4,15 @@ import App from './App.vue';
 import helpers from '@/utils/helpers';
 import CKEditor from '@ckeditor/ckeditor5-vue';
 import govVue3Components from '@dbca/gov-vue3-components';
-import 'datatables.net-bs5';
-import 'datatables.net-buttons-bs5';
-import 'datatables.net-responsive-bs5';
-import 'datatables.net-buttons/js/dataTables.buttons.js';
 const jsZip = require('jszip');
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 window.JSZip = jsZip;
-import 'datatables.net-buttons/js/buttons.html5.js';
 import 'select2';
 import 'sweetalert2/dist/sweetalert2.css';
 import '@dbca/gov-vue3-components/dist/library.css';
 import '@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import 'select2/dist/css/select2.min.css';
 import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css';
-import '@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css';
-import '@/../node_modules/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
 import '@/../node_modules/vue-multiselect/dist/vue-multiselect.css';
 
 // Add CSRF Token to every request
