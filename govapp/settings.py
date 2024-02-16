72309025b7d945c43a39d20d0db39d6e0271e1c7
Karsten Prehn <karsten.prehn@gmail.com>
2024-02-16 07:38:14 +0000

    Added app rest_framework_datatables

--- govapp/settings.py
+++ govapp/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     "govapp.apps.traffic",
     "govapp.apps.main",
     "rest_framework",
+    "rest_framework_datatables",
     "drf_spectacular",
     "django_filters",
     "django_cron",
@@ -199,6 +200,10 @@ VERSION_NO = "2.00"
 # https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework_datatables.renderers.DatatablesRenderer",
+    ),
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
