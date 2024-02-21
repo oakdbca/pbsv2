0662cdbff726baeb06babcd483dcbce5fd10a6c8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-21 16:37:06 +0800

    Add ContentTypeModelSerializer.

--- govapp/apps/main/serializer.py
+++ govapp/apps/main/serializer.py
@@ -1,7 +1,15 @@
 # Third-Party
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 from govapp.apps.main.models import District, Region
+
+
+class ContentTypeModelSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret["content_type"] = ContentType.objects.get_for_model(instance).id
+        return ret
 
 
 class RegionSerializer(serializers.ModelSerializer):
