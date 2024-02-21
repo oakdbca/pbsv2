f3c0089f4b8d884bc43779e29ebc4faf6016bbbd
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-21 11:17:32 +0800

    Import operational planning urls.

--- govapp/urls.py
+++ govapp/urls.py
@@ -66,6 +66,7 @@ urlpatterns = [
     # Include urls from other apps
     urls.path("", urls.include("govapp.apps.accounts.urls")),
     urls.path("", urls.include("govapp.apps.burnplanning.urls")),
+    urls.path("", urls.include("govapp.apps.operationalplanning.urls")),
     urls.path("", urls.include("govapp.apps.swagger.urls")),
     # Include api routes
     urls.path("api/", urls.include(router.urls)),
