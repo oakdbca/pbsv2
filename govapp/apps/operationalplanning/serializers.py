2f63c5e5c72b25420d93ebb3d99349d29becfe78
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-21 11:17:16 +0800

    Add OperationalPlanDatatableSerializer.

--- govapp/apps/operationalplanning/serializers.py
+++ govapp/apps/operationalplanning/serializers.py
@@ -5,6 +5,27 @@ from govapp.apps.operationalplanning.models import OperationalPlan
 
 
 class OperationalPlanSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = OperationalPlan
         fields = "__all__"
+
+
+class OperationalPlanDatatableSerializer(serializers.ModelSerializer):
+    year = serializers.CharField()
+    districts = serializers.ReadOnlyField()
+    regions = serializers.ReadOnlyField()
+    assigned_to_name = serializers.ReadOnlyField()
+
+    class Meta:
+        model = OperationalPlan
+        fields = [
+            "id",
+            "name",
+            "burn_plan_unit",
+            "year",
+            "regions",
+            "districts",
+            "status",
+            "assigned_to_name",
+        ]
