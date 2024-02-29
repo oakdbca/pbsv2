469cc064712b9df21895476f8e35c4e060352ac5
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-02-29 09:52:03 +0800

    Simplify uploading of comms log documents.

--- govapp/apps/logs/views.py
+++ govapp/apps/logs/views.py
@@ -1,7 +1,11 @@
+import logging
+
 from drf_spectacular import utils as drf_utils
 from rest_framework import viewsets
 
 from govapp.apps.logs import models, serializers
+
+logger = logging.getLogger(__name__)
 
 
 @drf_utils.extend_schema(tags=["Logs - Communications Logs"])
@@ -13,20 +17,14 @@ class CommunicationsLogEntryViewSet(viewsets.ModelViewSet):
     filterset_fields = ["content_type", "object_id"]
 
     def get_serializer_class(self):
-        """Return the serializer class."""
         if self.action == "create":
             return serializers.CommunicationsLogCreateEntrySerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         communications_log_entry = serializer.save(user=self.request.user)
-        # Save the files
-        for f in self.request.FILES.getlist("files"):
-            document = models.CommunicationsLogDocument()
-            document.entry = communications_log_entry
-            document.name = str(f)
-            document._file = f
-            document.save()
+        for file in self.request.FILES.getlist("files"):
+            communications_log_entry.documents.create(name=str(file), file=file)
         return communications_log_entry
 
 
