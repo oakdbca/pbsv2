420048970562b3d2511cc0457748495042f614dd
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-01 15:11:35 +0800

    Removed lines that are required to be added to the global urls file.

--- govapp/apps/main/urls.py
+++ govapp/apps/main/urls.py
@@ -1,13 +1,9 @@
 from django import urls
-from django.conf import settings
 from rest_framework import routers
 
 from govapp.apps.main import views
 
 router = routers.DefaultRouter()
-if settings.DEBUG is not True:
-    router.include_root_view = False
-
 router.register(r"regions", views.RegionViewSet, basename="regions")
 router.register(r"districts", views.DistrictViewSet, basename="districts")
 
