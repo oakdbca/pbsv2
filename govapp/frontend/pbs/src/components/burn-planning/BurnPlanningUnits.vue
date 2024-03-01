de6bd11d00dd12a494c8abacc793a7a9991dde51
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-01 12:06:50 +0800

    Add burn planning units components.

---
+++ govapp/frontend/pbs/src/components/burn-planning/BurnPlanningUnits.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div id="bp" class="container px-4">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="text-secondary">Burn Planing Units</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <DataTableBurnPlanUnits></DataTableBurnPlanUnits>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import DataTableBurnPlanUnits from '@/components/burn-planning/DataTableBurnPlanUnits.vue';
+
+export default {
+    name: 'BurnPlanning',
+    components: { DataTableBurnPlanUnits },
+};
+</script>
