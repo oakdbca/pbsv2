03a95250a31c5617219042d292e07af69c27ae45
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-01 12:20:27 +0800

    Fix typo.

--- govapp/frontend/pbs/src/components/burn-planning/BurnPlanningUnits.vue
+++ govapp/frontend/pbs/src/components/burn-planning/BurnPlanningUnits.vue
@@ -2,7 +2,7 @@
     <div id="bp" class="container px-4">
         <div class="row">
             <div class="col-12">
-                <h3 class="text-secondary">Burn Planing Units</h3>
+                <h3 class="text-secondary">Burn Planning Units</h3>
             </div>
         </div>
         <div class="row">
