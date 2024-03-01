94880292ec268c6be6c1c070514834702af4bca4
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-01 11:46:49 +0800

    Remove junk contents.

--- govapp/frontend/pbs/src/components/forms/BootstrapAccordion.vue
+++ govapp/frontend/pbs/src/components/forms/BootstrapAccordion.vue
@@ -7,33 +7,5 @@
 <script>
 export default {
     name: 'BootstrapAccordion',
-    props: {
-        title: {
-            type: String,
-            required: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-    },
-    data() {
-        return {
-            show: false,
-        };
-    },
-    computed: {
-        accordionClass() {
-            return {
-                'accordion-item': true,
-                show: this.show,
-            };
-        },
-    },
-    methods: {
-        toggle() {
-            this.show = !this.show;
-        },
-    },
 };
 </script>
