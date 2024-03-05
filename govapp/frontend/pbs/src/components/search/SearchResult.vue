a50b2993de16348263dfb290ec96768f6937730f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-05 11:42:13 +0800

    Add new search components.

---
+++ govapp/frontend/pbs/src/components/search/SearchResult.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <div class="card-header">{{ result.reference_number }}</div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+            </p>
+            <a :href="result.link" class="btn btn-primary">View Details Page</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchResult',
+    props: {
+        result: {
+            type: Object,
+            default: () => {},
+        },
+    },
+};
+</script>
---
+++ govapp/frontend/pbs/src/components/search/SearchResults.vue
@@ -0,0 +1,24 @@
+<template>
+    <SearchResult
+        v-for="result in results"
+        :key="result.id"
+        :result="result"
+    ></SearchResult>
+</template>
+
+<script>
+import SearchResult from './SearchResult.vue';
+
+export default {
+    name: 'SearchResults',
+    components: {
+        SearchResult,
+    },
+    props: {
+        results: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
