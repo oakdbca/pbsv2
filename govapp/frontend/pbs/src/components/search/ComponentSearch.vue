e5093db5a28a46b2fd9c336fe34ff39362fb9dfc
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-05 11:42:49 +0800

    Further work on search component.

--- govapp/frontend/pbs/src/components/search/ComponentSearch.vue
+++ govapp/frontend/pbs/src/components/search/ComponentSearch.vue
@@ -1,44 +1,81 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h3 class="text-secondary">
-                    Search<span v-if="query"> Results for '{{ query }}'</span>
-                </h3>
+    <LayoutDetails :loading="loading">
+        <template #heading>
+            <h3 class="text-secondary">
+                <template v-if="query">
+                    Your Search for '<em>{{ query }}</em
+                    >' returned {{ results.length }} result<template
+                        v-if="results.length != 1"
+                        >s</template
+                    >
+                </template>
+                <template v-else>Search</template>
+            </h3>
+        </template>
+        <template #left-menu>
+            <div class="card">
+                <div class="card-header">Advanced Search</div>
+                <div class="card-body border-bottom">
+                    <p class="card-text">
+                        <input
+                            v-model="query"
+                            class="form-control"
+                            type="search"
+                            autofocus
+                        />
+                    </p>
+                </div>
+                <div class="card-body">TODO: Add advanced search options</div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-3">
-                <div class="card">
-                    <div class="card-header">Advanced Search</div>
-                    <div class="card-body">
-                        <h4 class="card-title">Title</h4>
-                        <p class="card-text">Text</p>
-                    </div>
+        </template>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <SearchResults :results="results"></SearchResults>
                 </div>
             </div>
-            <div class="col-9">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <h4>Search Results</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
+    </LayoutDetails>
 </template>
 
 <script>
+import { utils, apiEndpoints } from '@/utils/hooks';
+
+import LayoutDetails from '../layout/LayoutDetails.vue';
+
+import SearchResults from './SearchResults.vue';
+
 export default {
     name: 'ComponentSearch',
+    components: {
+        LayoutDetails,
+        SearchResults,
+    },
+    data: function () {
+        return {
+            loading: true,
+            results: [],
+        };
+    },
     computed: {
         query: function () {
             return this.$route.query.q ? this.$route.query.q : '';
         },
     },
+    created: async function () {
+        if (this.query) {
+            await this.search();
+        }
+        this.loading = false;
+    },
+    methods: {
+        search: async function () {
+            await utils
+                .fetchUrl(apiEndpoints.search(this.query))
+                .then((data) => {
+                    this.results = data;
+                });
+        },
+    },
 };
 </script>
-
-<style lang="scss" scoped></style>
