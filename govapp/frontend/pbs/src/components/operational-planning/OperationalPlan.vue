0dd2fc6ae9a153269f24d0cdb87e1718c7fb390e
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-05 11:10:42 +0800

    Use new layout details component.

--- govapp/frontend/pbs/src/components/operational-planning/OperationalPlan.vue
+++ govapp/frontend/pbs/src/components/operational-planning/OperationalPlan.vue
@@ -1,70 +1,66 @@
 <template>
-    <div v-if="operationalPlan" class="container">
-        <div class="row">
-            <div class="col">
-                <h3>
-                    Operational Plan:
-                    <span class="text-secondary">{{
-                        operationalPlan.reference_number
-                    }}</span>
-                </h3>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-3">
-                <PanelLogs
-                    :communications-api-url="communicationsApiUrl"
-                    :post-communications-entry-api-url="
-                        postCommunicationsEntryApiUrl
-                    "
-                    :actions-api-url="actionsApiUrl"
-                    :content-type="operationalPlan.content_type"
-                    :object-id="operationalPlan.id"
-                />
-                <PanelWorkflow
-                    :status="operationalPlan.status"
-                    :status-display="operationalPlan.status_display"
-                    :content-type="operationalPlan.content_type"
-                    :pk="operationalPlan.id"
-                    :assignable-users="assignableUsers"
-                    :assign-to-me-api-url="assignToMeApiUrl"
-                    :assign-to-api-url="assignToApiUrl"
-                    :assigned-to="operationalPlan.assigned_to"
-                    :request-user-id="store.userData.id"
-                    @assign-to="assignTo"
-                ></PanelWorkflow>
-                <div class="card">
-                    <div class="card-header">Actions</div>
-                    <div class="card-body">
-                        <button class="btn btn-primary">Submit</button>
-                    </div>
+    <LayoutDetails :loading="!operationalPlan">
+        <template #heading>
+            <h3>
+                Operational Plan:
+                <span class="text-secondary">{{
+                    operationalPlan.reference_number
+                }}</span>
+            </h3>
+        </template>
+        <template #left-menu>
+            <PanelLogs
+                :communications-api-url="communicationsApiUrl"
+                :post-communications-entry-api-url="
+                    postCommunicationsEntryApiUrl
+                "
+                :actions-api-url="actionsApiUrl"
+                :content-type="operationalPlan.content_type"
+                :object-id="operationalPlan.id"
+            />
+            <PanelWorkflow
+                :status="operationalPlan.status"
+                :status-display="operationalPlan.status_display"
+                :content-type="operationalPlan.content_type"
+                :pk="operationalPlan.id"
+                :assignable-users="assignableUsers"
+                :assign-to-me-api-url="assignToMeApiUrl"
+                :assign-to-api-url="assignToApiUrl"
+                :assigned-to="operationalPlan.assigned_to"
+                :request-user-id="store.userData.id"
+                @assign-to="assignTo"
+            ></PanelWorkflow>
+            <div class="card">
+                <div class="card-header">Actions</div>
+                <div class="card-body">
+                    <button class="btn btn-primary">Submit</button>
                 </div>
             </div>
-            <div class="col">
-                <BootstrapAccordion id="operational-plan-accordion">
-                    <BootstrapAccordionItem
-                        heading="Overview One Two"
-                        icon-class="bi-exclamation-circle-fill"
-                        icon-color-class="text-warning"
-                    >
-                        Overview body
-                    </BootstrapAccordionItem>
-                    <BootstrapAccordionItem heading="Priority">
-                        Priority body
-                    </BootstrapAccordionItem>
-                    <BootstrapAccordionItem heading="Context">
-                        Context body
-                    </BootstrapAccordionItem>
-                </BootstrapAccordion>
-            </div>
-        </div>
-    </div>
+        </template>
+        <BootstrapAccordion id="operational-plan-accordion">
+            <BootstrapAccordionItem
+                heading="Overview One Two"
+                icon-class="bi-exclamation-circle-fill"
+                icon-color-class="text-warning"
+            >
+                Overview body
+            </BootstrapAccordionItem>
+            <BootstrapAccordionItem heading="Priority">
+                Priority body
+            </BootstrapAccordionItem>
+            <BootstrapAccordionItem heading="Context">
+                Context body
+            </BootstrapAccordionItem>
+        </BootstrapAccordion>
+    </LayoutDetails>
 </template>
 
 <script>
 import { useStore } from '@/stores/state';
 
 import { apiEndpoints, utils } from '@/utils/hooks';
+
+import LayoutDetails from '../layout/LayoutDetails.vue';
 
 import BootstrapAccordion from '../forms/BootstrapAccordion.vue';
 import BootstrapAccordionItem from '../forms/BootstrapAccordionItem.vue';
@@ -74,6 +70,7 @@ import PanelLogs from '../logging/PanelLogs.vue';
 export default {
     name: 'OperationalPlan',
     components: {
+        LayoutDetails,
         PanelLogs,
         BootstrapAccordion,
         BootstrapAccordionItem,
