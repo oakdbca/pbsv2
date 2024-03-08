da2f5b8973381c9c877ec7302244b4647bcbe2a4
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-08 13:50:09 +0800

    confgiure MessageAdmin.

--- govapp/apps/messaging/admin.py
+++ govapp/apps/messaging/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Message, MessageBatch
 
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ("user", "sender", "subject", "created")
+
+
 admin.site.register(MessageBatch)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
