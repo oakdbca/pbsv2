403e297d35a106bb57863ae623c83bfff518e641
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-08 14:31:02 +0800

    Further config of MessageBatchAdmin.

--- govapp/apps/messaging/admin.py
+++ govapp/apps/messaging/admin.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from .models import Message, MessageBatch
 
 
+class MessageBatchAdmin(admin.ModelAdmin):
+    model = MessageBatch
+    list_display = ("sender", "subject", "created")
+    filter_horizontal = (
+        "groups",
+        "users",
+    )
+
+
 class MessageAdmin(admin.ModelAdmin):
     model = Message
     list_display = ("user", "sender", "subject", "created")
 
 
-admin.site.register(MessageBatch)
+admin.site.register(MessageBatch, MessageBatchAdmin)
 admin.site.register(Message, MessageAdmin)
