8a473c6d4fedfb86d5edea69dba22b4f864cb6cc
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-08 11:46:11 +0800

    Check if a profile already exists as that is the case when creating users through the django admin interface.

--- govapp/apps/accounts/signals.py
+++ govapp/apps/accounts/signals.py
@@ -12,7 +12,8 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        if not hasattr(instance, "profile") or not instance.profile:
+            Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
