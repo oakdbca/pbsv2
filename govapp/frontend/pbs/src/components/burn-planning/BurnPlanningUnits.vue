c15fe9c6d26d78d20c93bb0def70a141a5f12a9e
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-11 14:33:52 +0800

    Add import and export modals to burn plan units dashboard.

--- govapp/frontend/pbs/src/components/burn-planning/BurnPlanningUnits.vue
+++ govapp/frontend/pbs/src/components/burn-planning/BurnPlanningUnits.vue
@@ -4,10 +4,18 @@
             <div class="col-12">
                 <h3 class="text-secondary">
                     Burn Planning Units
-                    <button class="btn btn-primary float-end">
+                    <button
+                        class="btn btn-primary float-end"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exportBurnPlanningUnits"
+                    >
                         Export <i class="bi bi-box-arrow-up"></i>
                     </button>
-                    <button class="btn btn-primary float-end me-2">
+                    <button
+                        class="btn btn-primary float-end me-2"
+                        data-bs-toggle="modal"
+                        data-bs-target="#importBurnPlanningUnits"
+                    >
                         Import <i class="bi bi-box-arrow-down"></i>
                     </button>
                 </h3>
@@ -23,13 +31,22 @@
             </div>
         </div>
     </div>
+    <ModalImportBurnPlanningUnits></ModalImportBurnPlanningUnits>
+    <ModalExportBurnPlanningUnits></ModalExportBurnPlanningUnits>
 </template>
 
 <script>
 import DataTableBurnPlanUnits from '@/components/burn-planning/DataTableBurnPlanUnits.vue';
 
+import ModalImportBurnPlanningUnits from '@/components/burn-planning/ModalImportBurnPlanningUnits.vue';
+import ModalExportBurnPlanningUnits from '@/components/burn-planning/ModalExportBurnPlanningUnits.vue';
+
 export default {
     name: 'BurnPlanning',
-    components: { DataTableBurnPlanUnits },
+    components: {
+        DataTableBurnPlanUnits,
+        ModalImportBurnPlanningUnits,
+        ModalExportBurnPlanningUnits,
+    },
 };
 </script>
---
+++ govapp/frontend/pbs/src/components/burn-planning/ModalExportBurnPlanningUnits.vue
@@ -0,0 +1,51 @@
+<template>
+    <div
+        id="exportBurnPlanningUnits"
+        class="modal fade"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="exportBurnPlanningUnitsLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1
+                        id="exportBurnPlanningUnitsLabel"
+                        class="modal-title fs-5"
+                    >
+                        Export Burn Planning Units
+                    </h1>
+                    <button
+                        type="button"
+                        class="btn-close btn-close-white"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">TODO: Add export options here.</div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                    >
+                        Cancel
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                        Export
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalExportBurnPlanningUnits',
+};
+</script>
+
+<style lang="scss" scoped></style>
